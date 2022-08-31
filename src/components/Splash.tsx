7abfe7b1aeb89e549820de1921e2afc24d0511dc
Bruno Lobão <brunowsl123@gmail.com>
2022-08-31 20:50:49 -0300

    ok

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,6 +1,7 @@
 export function Splash() {
     return (
-        <div className="bg-black max-w-md max-h-screen" style={{ height: "100vh" }}>
+        <div className="bg-black max-w-full max-h-screen content-center text-white" style={{ height: "100vh" }}>
+            <img src="/logo.png" />
         </div >
     )
 }
